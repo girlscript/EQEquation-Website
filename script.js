62a9ba8b0336655bc9d88a10d74fa817987793b3
sarthakkundra <sarthakkundra21@gmail.com>
2020-09-10 19:56:56 +0530

    migrated JS to script.js

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ const fadeEffect = setInterval(() => {
 
 // calling timer on DOM load
 window.addEventListener('load', fadeEffect);
+
+var menuBtn = document.getElementById("menuBtn");
+var sideNav = document.getElementById("sideNav");
+sideNav.style.right = "-250px"
+menuBtn.onclick = function(){
+  if(sideNav.style.right == "-250px"){
+    sideNav.style.right = "0"
+  }
+  else{
+    sideNav.style.right = "-250px"
+  }
+}
+
+var scroll = new SmoothScroll('a[href*="#"]',{
+  speed: 500,
+  speedAsDuration: true
+});
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("top-navbar").style.top = "0";
+  } else {
+    document.getElementById("top-navbar").style.top = "-80px";
+  }
+  prevScrollpos = currentScrollPos;
+}
